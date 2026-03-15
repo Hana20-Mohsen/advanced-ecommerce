9afe92ddb5e47811899acad5bacb6e320f379c7d
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-03-15 12:29:45 +0200

    update navbar import in frontend

--- frontend/src/layouts/MainLayOut.jsx
+++ frontend/src/layouts/MainLayOut.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar.jsx'
+import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayOut() {
