ce7fd1c00d7f9c388b37c80c30ac495811f2bb81
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-03-17 16:39:41 +0200

    try to solve issues in send email logic

--- backend/src/utilities/email/send.email.js
+++ backend/src/utilities/email/send.email.js
@@ -1,8 +1,46 @@
+// import nodemailer from "nodemailer";
+// import { asyncHandler } from "../error/error.js";
+
+// // Create a test account or replace with real credentials.
+// const sendEmail= asyncHandler(async({to='' , cc='' , bcc='', subject='E-commerce app' , text='' , html='' , attachments=[]}={})=>{
+//     const transporter = nodemailer.createTransport({
+//     service:'gmail',
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+//   tls:{
+//     rejectUnauthorized:false
+//   }
+// });
+// console.log(`transporter : ` , transporter);
+
+
+// // Wrap in an async IIFE so we can use await.
+// (async () => {
+//   const info = await transporter.sendMail({
+//     from: `"Hana mohsen" <${process.env.EMAIL}>`,
+//     to,
+//     cc,
+//     bcc,
+//     subject,
+//     text,
+//     html,
+//     attachments
+//   });
+//   console.log(`info : ` , info);
+  
+//   return info
+// })();
+// })
+
+// export default sendEmail
+
 import nodemailer from "nodemailer";
 import { asyncHandler } from "../error/error.js";
 
 // Create a test account or replace with real credentials.
-const sendEmail= asyncHandler(async({to='' , cc='' , bcc='', subject='E-commerce app' , text='' , html='' , attachments=[]}={})=>{
+const sendEmail= async({to='' , cc='' , bcc='', subject='E-commerce app' , text='' , html='' , attachments=[]}={})=>{
     const transporter = nodemailer.createTransport({
     service:'gmail',
   auth: {
@@ -32,6 +70,6 @@ console.log(`transporter : ` , transporter);
   
   return info
 })();
-})
+}
 
 export default sendEmail
