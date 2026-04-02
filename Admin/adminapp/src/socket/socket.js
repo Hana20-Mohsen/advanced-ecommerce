87146e748392745ddca12de616ef713e24edcd0c
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-02 16:12:51 +0200

    0

--- Admin/adminapp/src/socket/socket.js
+++ Admin/adminapp/src/socket/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client"
 import Cookies from "js-cookie";
 console.log(`${process.env.REACT_APP_BACKEND_URL}`);
-console.log(Cookies.get('token'));
+// console.log(Cookies.get('token'));
 
 const socket = io(`${process.env.REACT_APP_BACKEND_URL}`,{
  transports:["websocket"],
  auth:{
-  token: Cookies.get('token')
+  token: `adminToken`
  }
 })
 
