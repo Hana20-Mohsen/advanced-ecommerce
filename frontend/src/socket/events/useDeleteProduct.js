96aaa3b766b4528ec87ff3c9818828e385110761
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-04 13:17:47 +0200

    fix the bug

--- frontend/src/socket/events/useDeleteProduct.js
+++ frontend/src/socket/events/useDeleteProduct.js
@@ -9,16 +9,31 @@ const useDeleteProduct = () => {
         socket.on("delete-product", (deletedProduct) => {
             console.log("Socket received:", deletedProduct);
             toast.dark(`admin delete product`)
-            // queryClient.invalidateQueries(['getProducts'])
-            const data = queryClient.getQueryData(['getProducts']);
-            console.log("CACHE DATA:", data);
             queryClient.setQueryData(['getProducts'], (old) => {
-                //   console.log(' old data:', old);
+                if (!old?.data?.products) return old;
+
+                const updatedProducts = old.data.products.filter(
+                    (p) => p._id !== deletedProduct
+                );
+
+                return {
+                    ...old,
+                    data: {
+                        ...old.data,
+                        products: updatedProducts
+                    }
+                };
+            });
+            // queryClient.invalidateQueries(['getProducts'])
+            // const data = queryClient.getQueryData(['getProducts']);
+            // console.log("CACHE DATA:", data);
+            // queryClient.setQueryData(['getProducts'], (old) => {
+            //     //   console.log(' old data:', old);
 
 
-                // old?.filter(p => p._id !== deletedProduct)
-            }
-            );
+            //     // old?.filter(p => p._id !== deletedProduct)
+            // }
+            // );
         });
         return () => {
             socket.off("delete-product");
