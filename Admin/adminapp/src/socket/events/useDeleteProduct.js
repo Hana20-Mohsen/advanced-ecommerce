62c46d5dec90fe86399b3a16c98b278d64461b5a
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-04 12:31:36 +0200

    try to fix delete product socket

--- Admin/adminapp/src/socket/events/useDeleteProduct.js
+++ Admin/adminapp/src/socket/events/useDeleteProduct.js
@@ -3,18 +3,21 @@ import socket from "../socket.js";
 import { useQueryClient } from "react-query";
 import { toast } from "react-toastify";
 
-const useDeleteProduct = ()=>{
+const useDeleteProduct = () => {
     const queryClient = useQueryClient();
-    useEffect(()=>{
-        socket.on("delete-product" , (deletedProduct)=>{
+    useEffect(() => {
+        socket.on("delete-product", (deletedProduct) => {
             console.log("Socket received:", deletedProduct);
             toast.dark(`admin delete product`)
-                queryClient.invalidateQueries(['getProducts'])
+            // queryClient.invalidateQueries(['getProducts'])
+            queryClient.setQueryData(['getProducts'], (old) =>
+                old?.filter(p => p._id !== deletedProduct)
+            );
         });
-        return ()=>{
+        return () => {
             socket.off("delete-product");
         }
-    }, []) 
+    }, [])
 }
 
 export default useDeleteProduct;
